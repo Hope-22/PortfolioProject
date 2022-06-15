becf5c4fb59840afea2c6fa47028cf638cc4141d
Hope-22 <onofehope107@gmail.com>
2022-06-15 15:46:07 +0100

    Add project files.

---
+++ Portfolio Project/Models/user.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio_Project.Models
+{
+    public class user
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string JobRole { get; set; }
+        
+    }
+}
